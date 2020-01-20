e4ef28fbe3a3d8cf62db89a639400e6dcf8afa66
Nick Nifontov <Nick Nifontov@Win10-AIR.localdomain>
2020-01-21 00:17:00 +0200

    updated QuarkTS

--- Core/QuarkTS/src/os/include/qtasks.h
+++ Core/QuarkTS/src/os/include/qtasks.h
@@ -15,7 +15,7 @@
     #endif  
 
     #if ( Q_ATCOMMAND_PARSER == 1)
-        #include "qatparser.h"
+        #include "qatcli.h"
     #endif
 
     #ifdef __cplusplus
@@ -230,6 +230,7 @@
 
     qBool_t qTaskSendNotification( qTask_t * const Task, void* eventdata);
     qBool_t qTaskQueueNotification( qTask_t * const Task, void* eventdata );
+    qBool_t qTask_HasPendingNotifications( const qTask_t * const Task  );
     qState_t qTaskGetState( const qTask_t * const Task);
     #if ( Q_TASK_COUNT_CYCLES == 1 )
         qCycles_t qTaskGetCycles( const qTask_t * const Task );   
